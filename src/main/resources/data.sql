d3eb6ee44faf6c8c0f6a40e8ee1aa9a451e4364a
Valar <a.valarpirai@gmail.com>
2024-04-03 22:21:45 +0530

    Load default users

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT into users(first_name, last_name, email) VALUES
+('Test1', 'User', 'test1@gmail.com'),
+('Test2', 'User', 'test2@gmail.com'),
+('Test3', 'User', 'test3@gmail.com'),
+('Valar', 'User', 'valar@gmail.com');
